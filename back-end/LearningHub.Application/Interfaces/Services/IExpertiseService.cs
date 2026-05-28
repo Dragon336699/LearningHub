db7f7ece55c41e3fc7b49b8d85fb367c9ce1d380
Bảo Long <anhruoia1a1@gmail.com>
2026-05-28 13:46:30 +0700

    feat(be): add get all expertises
    - Add API get all expertises
    - Fix search users by keyword
    - Remove avatar field in UpdateUserProfileCommand

---
+++ back-end/LearningHub.Application/Interfaces/Services/IExpertiseService.cs
@@ -0,0 +1,10 @@
+﻿using LearningHub.Application.Common;
+using LearningHub.Application.Dtos.Expertises;
+
+namespace LearningHub.Application.Interfaces.Services
+{
+    public interface IExpertiseService
+    {
+        Task<Result<List<ExpertiseDto>>> GetAllExpertisesAsync();
+    }
+}
