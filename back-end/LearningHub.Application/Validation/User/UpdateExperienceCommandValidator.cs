596bc963511affe9b852e86cf541ef9c6afd1358
Bảo Long <anhruoia1a1@gmail.com>
2026-06-03 00:31:04 +0700

    fix(be): add validation for update user experience

---
+++ back-end/LearningHub.Application/Validation/User/UpdateExperienceCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using LearningHub.Application.Dtos.Users;
+
+namespace LearningHub.Application.Validation.User
+{
+    public class UpdateExperienceCommandValidator : AbstractValidator<UpdateExperienceCommand>
+    {
+        public UpdateExperienceCommandValidator()
+        {
+            RuleFor(e => e.Title)
+                .Must(t => !string.IsNullOrWhiteSpace(t)).WithMessage("Experience title is required")
+                .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+
+            RuleFor(e => e.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(x => x.StartDate)
+                .LessThanOrEqualTo(DateTime.UtcNow)
+                .WithMessage("StartDate cannot be in the future.");
+
+            RuleFor(x => x)
+                .Must(x => x.StartDate <= x.EndDate)
+                .WithMessage("StartDate must be less than or equal to EndDate.");
+        }
+    }
+}
